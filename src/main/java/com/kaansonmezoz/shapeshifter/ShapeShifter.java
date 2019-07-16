e2dd9a81fa8de2ed690d1a73494799dd6af537a5
Kaan <ksonmezoz@outlook.com>
2019-07-16 23:30:37 +0300

    Additional readings have been added to todo part

--- src/main/java/com/kaansonmezoz/shapeshifter/ShapeShifter.java
+++ src/main/java/com/kaansonmezoz/shapeshifter/ShapeShifter.java
@@ -49,6 +49,26 @@ public class ShapeShifter {
         //TODO: Access modifierlara gore set etme islemi yapilacak
         //TODO: Once accessible yap ilgil field'ı sonra veriyi set et. Ardindan tekrar eski haline geri getir !
 
+        /*
+                ----------------------- USEFUL RESOURCES ----------------------------
+
+                https://stackoverflow.com/questions/3301635/change-private-static-final-field-using-java-reflection
+                https://www.google.com/search?q=field+how+to+set+modifier+java&oq=field+how+to+set+modifier+java&aqs=chrome..69i57.6868j0j1&sourceid=chrome&ie=UTF-8
+                https://stackoverflow.com/questions/39053175/how-to-find-the-access-modifier-of-a-member-using-java-reflection
+                http://tutorials.jenkov.com/java/access-modifiers.html
+                https://www.google.com/search?q=field+get+access+modifier+java&oq=field+get+access+modifier+java&aqs=chrome..69i57j33l2.4916j1j1&sourceid=chrome&ie=UTF-8
+                https://www.tutorialspoint.com/javareflect/javareflect_field_getmodifiers.htm
+                https://www.google.com/search?q=field+getmodifiers&oq=field.getMod&aqs=chrome.1.69i57j0l5.6020j0j1&sourceid=chrome&ie=UTF-8
+                https://stackoverflow.com/questions/13400075/reflection-generic-get-field-value
+                https://www.google.com/search?q=get+field+value+reflection+java&oq=get+field+value+reflection+java&aqs=chrome..69i57j0l5.5606j0j1&sourceid=chrome&ie=UTF-8
+                https://stackoverflow.com/questions/32716952/set-private-field-value-with-reflection
+                https://www.google.com/search?q=how+to+set+private+field+reflection+java&oq=how+to+set+private+field+reflection+java&aqs=chrome..69i57j0l3.5853j0j1&sourceid=chrome&ie=UTF-8
+                https://stackoverflow.com/questions/18852059/java-list-containsobject-with-field-value-equal-to-x
+
+
+
+         */
+
         int accessModifier = targetField.getModifiers();
 
         targetField.set(targetObject, sourceField.get(sourceObject));
