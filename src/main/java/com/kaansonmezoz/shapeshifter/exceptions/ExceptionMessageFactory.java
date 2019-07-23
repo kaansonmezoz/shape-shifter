8173e38e50646ec5b960fcc1f59e48002b0fa9f5
kaan.sonmezoz <kaan.sonmezoz@trendyol.com>
2019-07-23 11:42:24 +0300

    New exception has been defined

--- src/main/java/com/kaansonmezoz/shapeshifter/exceptions/ExceptionMessageFactory.java
+++ src/main/java/com/kaansonmezoz/shapeshifter/exceptions/ExceptionMessageFactory.java
@@ -3,7 +3,11 @@ package com.kaansonmezoz.shapeshifter.exceptions;
 public class ExceptionMessageFactory {
     public String getExceptionMessageFor(ErrorType errorType, String ... args){
         if(errorType == ErrorType.NoSuchFieldInTargetObject){
-            return "No such field " + args[0] + " found in target class " + args[1];
+            return "No such field " + args[0] + " found in target class " + args[1] + "\n" +
+                   "Error message: " + args[2];
+        }
+        else if(errorType == ErrorType.IllegalAccessToPrivateField){
+            return args[0];
         }
         return null;
     }
